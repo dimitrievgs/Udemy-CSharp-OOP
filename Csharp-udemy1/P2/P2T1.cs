0bcf7d1265861dc64aa01b227da33fa691eeaa1d
dimitrievgs <dimitriev.gs@mail.ru>
2022-01-23 18:11:18 +0300

    Add project files.

---
+++ Csharp-udemy1/P2/P2T1.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Friendly conversation
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        Random rand = new Random();
+        string appName = RandName(rand, rand.Next(3, 7));
+        int appYear = rand.Next(1950, 2010);
+        int appAge = DateTime.Now.Year - appYear;
+
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Nice to meet you {name}. My name is {appName}, How old are you?");
+        int.TryParse(Console.ReadLine(), out int age);
+        Console.WriteLine($"{age} is good. As I was born in " + appYear
+            + " so I am " + appAge + " years old.");
+    }
+
+    private static char RandChar(Random rand)
+    {
+        char c = (char)((byte)('A') + rand.Next(0, 25));
+        return c;
+    }
+
+    private static string RandName(Random rand, int charsNumber)
+    {
+        string name = "";
+        for (int i = 0; i < charsNumber; i++)
+            name += RandChar(rand);
+        return name;
+    }
+}
---
+++ Csharp-udemy1/P3/P3T4.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Upside Down Triangle
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        const int top = 10;
+        for (int i = 0; i < top; i++)
+        {
+            for (int j = 0; j < top - i; j++)
+            {
+                Console.Write("0 ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
