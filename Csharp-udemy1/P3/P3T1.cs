627681c652ad2acbf2ef50923bea6e8be0bb1243
dimitrievgs <dimitriev.gs@mail.ru>
2022-01-24 14:24:13 +0300

    +

---
+++ Csharp-udemy1/P3/P3T1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        const int top = 10;
+        for (int i = 0; i < top; i++)
+        {
+            for (int j = 0; j < top - i; j++)
+            {
+                Console.Write("0 ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
