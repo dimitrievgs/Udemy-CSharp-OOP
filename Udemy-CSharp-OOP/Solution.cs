2c4894a2d127103adcf916181fc3eaa95c22de1c
dimitrievgs <dimitriev.gs@mail.ru>
2022-02-10 21:46:01 +0300

    Main(...) indentation fix

--- Udemy-CSharp-OOP/Solution.cs
+++ Udemy-CSharp-OOP/Solution.cs
@@ -5,8 +5,7 @@ using System.Linq;
 
 public class Solution
 {
-    public static void 
-        Main(string[] args)
+    public static void Main(string[] args)
     {
         P10T24.Solution.Main(args);
     }
